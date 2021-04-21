31fe8ab9931a52f4ab98707006f019d586d6e733
Ming Cheng <cmgrace@bu.edu>
2021-04-20 21:53:18 -0400

    function added

---
+++ src/features/todoSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todoList: [],
+};
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    saveTodo: (state, action) => {
+      state.todoList.push(action.payload);
+    },
+    setCheck: (state, action) => {
+      state.todoList.map((item) => {
+        if (action.payload === item.id) {
+          if (item.done === true) item.done = false;
+          else if (item.done === false) item.done = true;
+        }
+      });
+    },
+  },
+});
+
+export const { saveTodo, setCheck } = todoSlice.actions;
+export const selectTodoList = (state) => state.todos.todoList;
+export default todoSlice.reducer;
